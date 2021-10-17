c9b34152631743e4b856e8045a7721f11a8c804d
Sajal Gupta <guptasajal411@gmail.com>
2021-10-17 16:06:03 +0530

    feat: :rocket: Add colourCombination to User model

--- models/userModel.js
+++ models/userModel.js
@@ -4,13 +4,10 @@ require('dotenv').config();
 mongoose.connect("mongodb+srv://" + process.env.usernameMongoDB + ":" + process.env.password + "@cluster0.xgjts.mongodb.net/authCodeDB");
 
 const userSchema = new mongoose.Schema({
-    email: String
+    email: String,
+    colourCombination: String
 });
 
 const User = new mongoose.model("User", userSchema);
-
-//create a new user in User model
-// const newBook = new User({ email: "email@example.com" });
-// newBook.save();
 
 module.exports = User;
