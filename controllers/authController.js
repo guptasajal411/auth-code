1d2af315515a206e96cb1a2a67fcfcc325fa381f
Sajal Gupta <guptasajal411@gmail.com>
2021-11-13 12:03:33 +0530

    fix: :rocket: Check if user exists before login

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,34 +26,30 @@ exports.postLogin = function (req, res) {
         if (err) {
             res.send(err);
         } else {
-            if (foundUser == null){
-                res.send(`Please register before login <a href="/registration">here</a>`);
+            if (foundUser == null) {
+                res.render("login", { message: "emailNotFound" });
             } else {
-                if (foundUser) {
-                    var transporter = nodemailer.createTransport({
-                        service: 'gmail',
-                        auth: {
-                            user: process.env.email,
-                            pass: process.env.emailPassword
-                        },
-                        tls: {rejectUnauthorized: false}
-                    });
-                    var mailOptions = {
-                        from: process.env.email,
-                        to: foundUser.email,
-                        subject: 'Your OTP by Auth Code',
-                        html: '<h1>OTP by Auth Code</h1>OTP is: ' + otpGenerator.generate(6, { upperCase: false, specialChars: false, alphabets: false, digits: true })
-                    };
-                    transporter.sendMail(mailOptions, function (error, info) {
-                        if (error) {
-                            res.send(error);
-                        } else {
-                            res.render("login",{message:"Email sent! Check your inbox for OTP"});
-                        }
-                    });
-                } else {
-                    res.render("login",{message:"emailNotFound"});
-                }
+                var transporter = nodemailer.createTransport({
+                    service: 'gmail',
+                    auth: {
+                        user: process.env.email,
+                        pass: process.env.emailPassword
+                    },
+                    tls: { rejectUnauthorized: false }
+                });
+                var mailOptions = {
+                    from: process.env.email,
+                    to: foundUser.email,
+                    subject: 'Your OTP by Auth Code',
+                    html: '<h1>OTP by Auth Code</h1>OTP is: ' + otpGenerator.generate(6, { upperCase: false, specialChars: false, alphabets: false, digits: true })
+                };
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (error) {
+                        res.send(error);
+                    } else {
+                        res.render("login", { message: "Email sent! Check your inbox for OTP" });
+                    }
+                });
             }
         }
     });
